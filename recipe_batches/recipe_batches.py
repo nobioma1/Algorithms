e7fd5ba16e7e671af073a418a3721bcd56d0a4ba
Noble Obioma <nobioma1@gmail.com>
2019-10-09 15:59:46 +0100

    implement receipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,25 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    batch_count = 0
+    while True:
+        for i in recipe.keys():
+            if i in ingredients:
+                if ingredients[i] < recipe[i]:
+                    return batch_count
+                else:
+                    ingredients[i] = ingredients[i] - recipe[i]
+            else:
+                return batch_count
+        batch_count += 1
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
